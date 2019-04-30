9e73a9227fe7ff299f55adaf77b0f3257259106f
Cristian Vargas Flores <criz.241095@gmail.com>
2019-04-30 11:14:43 -0500

    Se configura el service worker y se agrega una pagina de offline

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    console.log( " actualizacion " );
+    //console.log( " actualizacion2 " );
+    //console.log("hola" );
+});
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,44 @@
-console.log('Se agrega el archivo para el service worker');
+
+var CACHE_NAME = 'prueba-cache-v1';
+var urlsToCache = [
+  'img/portfolio/thumbnails/1.jpg',
+  'img/portfolio/thumbnails/2.jpg',
+  'img/portfolio/thumbnails/3.jpg',
+  'img/portfolio/thumbnails/4.jpg',
+  'img/portfolio/thumbnails/5.jpg',
+  'img/portfolio/thumbnails/6.jpg',
+  'js/index.js',
+  'internet.html',
+  'css/estilos.css'
+];
+
+/* Al cargar la pagina se guardan en cache los archivos. */
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        var fetchPromise = fetch(event.request).then(function(networkResponse) { 
+          if(response != undefined) {
+            cache.put(event.request, networkResponse.clone());
+          }
+          return networkResponse;
+        }).catch(error => {
+          console.log('Fetch failed: ' + error);
+          return caches.match('internet.html');
+        });
+        return response || fetchPromise;
+      })
+    })
+  );
+});
